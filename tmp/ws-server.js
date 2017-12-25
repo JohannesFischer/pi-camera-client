62ac960bee22854a9c805d82cdfefdf3c11b5bc0
Johannes Fischer <johannes.fischer@rakuten.com>
2017-12-25 11:34:43 +0900

    add semicolon, update packages

--- tmp/ws-server.js
+++ tmp/ws-server.js
@@ -1,7 +1,5 @@
-/* eslint-disable brace-style, no-console */
-
 const WebSocket = require('ws');
-const port = 9090; //Config.ws.port;
+const port = 9090; // Config.ws.port;
 const wss = new WebSocket.Server({ port });
 
 console.log('WS server started on ws://0.0.0.0:%s', port);
